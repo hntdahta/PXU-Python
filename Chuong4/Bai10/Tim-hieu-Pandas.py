93eb3c8e4a2c0bb89538831787bf0cd81ab6cac7
hntdahta <ngthnhdat@gmail.com>
2021-01-09 14:27:30 +0700

    Upload C4

---
+++ Chuong4/Bai10/Tim-hieu-Pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+a = pd.read_excel("E:\Python\Book1.xlsx")
+a1 = a.head(10)
+print(a)
+
+url = r'https://www1.ncdc.noaa.gov/pub/data/cdo/samples/PRECIP_HLY_sample_csv.csv'
+df = pd.read_csv(url)
+df1 = df.head(10)
+print(df1)
+
+js = pd.read_json("https://raw.githubusercontent.com/domoritz/maps/master/data/iris.json")
+js1 = js.head(10)
+print(js1)
